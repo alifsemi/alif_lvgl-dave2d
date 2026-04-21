8cff4e9673d8b9657d6300166ac83276c8e132c7
Teppo Jarvelin <teppo.jarvelin@alifsemi.com>
2026-04-21 11:04:31 +0300

    Updated version numbers

--- gen_pack.sh
+++ gen_pack.sh
@@ -62,8 +62,8 @@ DEFAULT_ARGS="-v"
 # Default: empty
 #
 PACKCHK_DEPS="
-  https://github.com/lvgl/lvgl/raw/v9.1.0/env_support/cmsis-pack/LVGL.lvgl.pdsc
-  https://github.com/alifsemi/alif_dave2d-driver/releases/download/v1.0.1/AlifSemiconductor.Dave2DDriver.pdsc
+  https://github.com/lvgl/lvgl/raw/v9.5.0/env_support/cmsis-pack/LVGL.lvgl.pdsc
+  https://github.com/alifsemi/alif_dave2d-driver/releases/download/v2.0.2/AlifSemiconductor.Dave2DDriver.pdsc
 "
 
 # Optional: restrict fallback modes for changelog generation
