f3adcab039ceece64ff41fb865ee91333ac9e4e7
Kimmo Vaisanen <kimmo.vaisanen@alifsemi.com>
2026-04-27 15:44:12 +0300

    Fix NULL pointer check

--- lv_dave2d/src/lv_draw_dave2d_label.c
+++ lv_dave2d/src/lv_draw_dave2d_label.c
@@ -1,6 +1,7 @@
 #include "lv_draw_dave2d.h"
 #include "src/draw/lv_draw_label_private.h"
 #include "src/misc/lv_area_private.h"
+#include <string.h>
 
 static void lv_draw_dave2d_draw_letter_cb(lv_draw_unit_t * draw_unit, lv_draw_glyph_dsc_t * glyph_draw_dsc,
                                           lv_draw_fill_dsc_t * fill_draw_dsc, const lv_area_t * fill_area);
@@ -109,138 +110,140 @@ void lv_draw_dave2d_label(lv_draw_dave2d_unit_t * u, const lv_draw_label_dsc_t *
 static void lv_draw_dave2d_draw_letter_cb(lv_draw_unit_t * u, lv_draw_glyph_dsc_t * glyph_draw_dsc,
                                           lv_draw_fill_dsc_t * fill_draw_dsc, const lv_area_t * fill_area)
 {
-    d2_u8 current_fillmode;
-    lv_area_t clip_area;
-    lv_area_t letter_coords;
+    if (glyph_draw_dsc) {
+        d2_u8 current_fillmode;
+        lv_area_t clip_area;
+        lv_area_t letter_coords;
 
-    int32_t x;
-    int32_t y;
+        int32_t x;
+        int32_t y;
 
-    letter_coords = *glyph_draw_dsc->letter_coords;
+        letter_coords = *glyph_draw_dsc->letter_coords;
 
-    bool is_common;
+        bool is_common;
 #if D2_LABEL_RENDER_EACH_LETTER
-    is_common = lv_area_intersect(&clip_area, glyph_draw_dsc->letter_coords, u->clip_area);
+        is_common = lv_area_intersect(&clip_area, glyph_draw_dsc->letter_coords, u->clip_area);
 #else
-    is_common = lv_area_intersect(&clip_area, glyph_draw_dsc->letter_coords, unit->label_coords);
+        is_common = lv_area_intersect(&clip_area, glyph_draw_dsc->letter_coords, unit->label_coords);
 #endif
-    if(!is_common) return;
+        if(!is_common) return;
 
 #if D2_LABEL_RENDER_EACH_LETTER
-    x = 0 - unit->base_unit.target_layer->buf_area.x1;
-    y = 0 - unit->base_unit.target_layer->buf_area.y1;
+        x = 0 - unit->base_unit.target_layer->buf_area.x1;
+        y = 0 - unit->base_unit.target_layer->buf_area.y1;
 #else
-    x = 0 - unit->label_coords->x1;
-    y = 0 - unit->label_coords->y1;
+        x = 0 - unit->label_coords->x1;
+        y = 0 - unit->label_coords->y1;
 #endif
 
-    lv_area_move(&clip_area, x, y);
-    lv_area_move(&letter_coords, x, y);
+        lv_area_move(&clip_area, x, y);
+        lv_area_move(&letter_coords, x, y);
 
 #if LV_USE_OS
-    lv_result_t  status;
-    status = lv_mutex_lock(unit->pd2Mutex);
-    LV_ASSERT(LV_RESULT_OK == status);
+        lv_result_t  status;
+        status = lv_mutex_lock(unit->pd2Mutex);
+        LV_ASSERT(LV_RESULT_OK == status);
 #endif
 
 #if D2_LABEL_RENDER_EACH_LETTER
 #if D2_RENDER_EACH_OPERATION
 #if (D2_USE_INTERNAL_RENDERBUFFERS == 0)
-    d2_selectrenderbuffer(unit->d2_handle, unit->renderbuffer);
+        d2_selectrenderbuffer(unit->d2_handle, unit->renderbuffer);
 #endif
 #endif
 
-    //
-    // Generate render operations
-    //
+        //
+        // Generate render operations
+        //
 
-    d2_framebuffer_from_layer(unit->d2_handle, unit->base_unit.target_layer);
+        d2_framebuffer_from_layer(unit->d2_handle, unit->base_unit.target_layer);
 
-    current_fillmode = d2_getfillmode(unit->d2_handle);
+        current_fillmode = d2_getfillmode(unit->d2_handle);
 
-    d2_cliprect(unit->d2_handle, (d2_border)clip_area.x1, (d2_border)clip_area.y1, (d2_border)clip_area.x2,
-                (d2_border)clip_area.y2);
+        d2_cliprect(unit->d2_handle, (d2_border)clip_area.x1, (d2_border)clip_area.y1, (d2_border)clip_area.x2,
+                    (d2_border)clip_area.y2);
 #endif
 
-    if(glyph_draw_dsc) {
-        switch(glyph_draw_dsc->format) {
-            case LV_FONT_GLYPH_FORMAT_NONE: {
+        if(glyph_draw_dsc) {
+            switch(glyph_draw_dsc->format) {
+                case LV_FONT_GLYPH_FORMAT_NONE: {
 #if LV_USE_FONT_PLACEHOLDER
-                    /* Draw a placeholder rectangle*/
-                    lv_draw_border_dsc_t border_draw_dsc;
-                    lv_draw_border_dsc_init(&border_draw_dsc);
-                    border_draw_dsc.opa = glyph_draw_dsc->opa;
-                    border_draw_dsc.color = glyph_draw_dsc->color;
-                    border_draw_dsc.width = 1;
-                    //lv_draw_sw_border(u, &border_draw_dsc, glyph_draw_dsc->bg_coords);
-                    lv_draw_dave2d_border(unit, &border_draw_dsc, glyph_draw_dsc->bg_coords);
+                        /* Draw a placeholder rectangle*/
+                        lv_draw_border_dsc_t border_draw_dsc;
+                        lv_draw_border_dsc_init(&border_draw_dsc);
+                        border_draw_dsc.opa = glyph_draw_dsc->opa;
+                        border_draw_dsc.color = glyph_draw_dsc->color;
+                        border_draw_dsc.width = 1;
+                        //lv_draw_sw_border(u, &border_draw_dsc, glyph_draw_dsc->bg_coords);
+                        lv_draw_dave2d_border(unit, &border_draw_dsc, glyph_draw_dsc->bg_coords);
 #endif
-                }
-                break;
-            case LV_FONT_GLYPH_FORMAT_A1 ... LV_FONT_GLYPH_FORMAT_A8:
+                    }
+                    break;
+                case LV_FONT_GLYPH_FORMAT_A1 ... LV_FONT_GLYPH_FORMAT_A8:
 #if D2_LABEL_RENDER_EACH_LETTER
-                {
-                    lv_draw_buf_t * draw_buf = glyph_draw_dsc->glyph_data;
+                    {
+                        lv_draw_buf_t * draw_buf = glyph_draw_dsc->glyph_data;
 
-                    lv_label_render(unit->d2_handle, &glyph_draw_dsc->color, glyph_draw_dsc->opa, &letter_coords, draw_buf);
+                        lv_label_render(unit->d2_handle, &glyph_draw_dsc->color, glyph_draw_dsc->opa, &letter_coords, draw_buf);
 
-                    d2_setfillmode(unit->d2_handle, current_fillmode);
-                }
+                        d2_setfillmode(unit->d2_handle, current_fillmode);
+                    }
 #else
-                {
-                    lv_draw_buf_t * draw_buf = glyph_draw_dsc->glyph_data;
+                    {
+                        lv_draw_buf_t * draw_buf = glyph_draw_dsc->glyph_data;
 
-                    lv_area_t letter_area;
-                    letter_area.x1 = clip_area.x1 - letter_coords.x1;
-                    letter_area.x2 = letter_area.x1 + clip_area.x2 - clip_area.x1;
-                    letter_area.y1 = clip_area.y1 - letter_coords.y1;
-                    letter_area.y2 = letter_area.y1 + clip_area.y2 - clip_area.y1;
+                        lv_area_t letter_area;
+                        letter_area.x1 = clip_area.x1 - letter_coords.x1;
+                        letter_area.x2 = letter_area.x1 + clip_area.x2 - clip_area.x1;
+                        letter_area.y1 = clip_area.y1 - letter_coords.y1;
+                        letter_area.y2 = letter_area.y1 + clip_area.y2 - clip_area.y1;
 
-                    lv_draw_buf_copy(unit->label_drawbuffer, &clip_area, draw_buf, &letter_area);
-                }
+                        lv_draw_buf_copy(unit->label_drawbuffer, &clip_area, draw_buf, &letter_area);
+                    }
 #endif
-                break;
-            case LV_FONT_GLYPH_FORMAT_IMAGE: {
+                    break;
+                case LV_FONT_GLYPH_FORMAT_IMAGE: {
 #if LV_USE_IMGFONT
-                    lv_draw_image_dsc_t img_dsc;
-                    lv_draw_image_dsc_init(&img_dsc);
-                    img_dsc.rotation = 0;
-                    img_dsc.scale_x = LV_SCALE_NONE;
-                    img_dsc.scale_y = LV_SCALE_NONE;
-                    img_dsc.opa = glyph_draw_dsc->opa;
-                    img_dsc.src = glyph_draw_dsc->glyph_data;
-                    //lv_draw_sw_image(draw_unit, &img_dsc, glyph_draw_dsc->letter_coords);
+                        lv_draw_image_dsc_t img_dsc;
+                        lv_draw_image_dsc_init(&img_dsc);
+                        img_dsc.rotation = 0;
+                        img_dsc.scale_x = LV_SCALE_NONE;
+                        img_dsc.scale_y = LV_SCALE_NONE;
+                        img_dsc.opa = glyph_draw_dsc->opa;
+                        img_dsc.src = glyph_draw_dsc->glyph_data;
+                        //lv_draw_sw_image(draw_unit, &img_dsc, glyph_draw_dsc->letter_coords);
 #endif
-                }
-                break;
-            default:
-                break;
+                    }
+                    break;
+                default:
+                    break;
+            }
         }
-    }
 
-    //
-    // Execute render operations
-    //
+        //
+        // Execute render operations
+        //
 #if D2_LABEL_RENDER_EACH_LETTER
 #if D2_RENDER_EACH_OPERATION
 #if D2_USE_INTERNAL_RENDERBUFFERS
-    d2_start_rendering();
+        d2_start_rendering();
 #else
-    d2_executerenderbuffer(unit->d2_handle, unit->renderbuffer, 0);
-    d2_flushframe(unit->d2_handle);
+        d2_executerenderbuffer(unit->d2_handle, unit->renderbuffer, 0);
+        d2_flushframe(unit->d2_handle);
 #endif
 #endif
 #endif
+
+#if LV_USE_OS
+        status = lv_mutex_unlock(unit->pd2Mutex);
+        LV_ASSERT(LV_RESULT_OK == status);
+#endif
+    }
 
     if(fill_draw_dsc && fill_area) {
         //lv_draw_sw_fill(u, fill_draw_dsc, fill_area);
         lv_draw_dave2d_fill(unit, fill_draw_dsc, fill_area);
     }
-
-#if LV_USE_OS
-    status = lv_mutex_unlock(unit->pd2Mutex);
-    LV_ASSERT(LV_RESULT_OK == status);
-#endif
 }
 
 static void lv_label_render(d2_device * handle, const lv_color_t * color, lv_opa_t opa,
