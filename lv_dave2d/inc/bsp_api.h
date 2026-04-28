eac690d59b1f2275d98c4c0e1e0edce7c435131e
Teppo Jarvelin <teppo.jarvelin@alifsemi.com>
2026-04-28 15:06:18 +0300

    LVGL v9.5.0 support. Dave2D Driver 2.0.3.

--- lv_dave2d/inc/bsp_api.h
+++ lv_dave2d/inc/bsp_api.h
@@ -1,15 +1,14 @@
 /**
- * @file hal_data.h
+ * @file bsp_api.h
  *
  * @brief   This is a stub file that is included from LVGL's internal lv_draw_dave2d.h
  *          and is needed to be able to use this pack without modifying LVGL
  *
  */
 
-#ifndef HAL_DATA_H
-#define HAL_DATA_H
+#ifndef BSP_API_H
+#define BSP_API_H
 
 #include "dave_driver.h"
 
-#endif /*HAL_DATA_H*/
-
+#endif /*BSP_API_H*/
